d436d6b27c698630fb8ca1b4a2b77b6ccc59c498
tekofx <tekofxx@gmail.com>
2023-07-27 09:32:16 +0200

    Removed components

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,8 +3,6 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
 import Gallery from '../components/Gallery';
 export default function Home() {
   return (
@@ -22,8 +20,6 @@ export default function Home() {
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
