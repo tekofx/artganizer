926d5f29f4cdb4861c5ddc83ea3fb7bf4230865b
tekofx <tekofxx@gmail.com>
2023-07-28 10:27:35 +0200

    Added color

--- backend/src/routes/labels.ts
+++ backend/src/routes/labels.ts
@@ -21,8 +21,8 @@ router.get('/:id', async (req: Request, res: Response) => {
 
 
 router.post('/', async (req: Request, res: Response) => {
-    const { name } = req.body;
-    const label = LabelRepo.create({ name });
+    const { name, color } = req.body;
+    const label = LabelRepo.create({ name: name, color: color });
     await LabelRepo.save(label);
     var labels = await LabelRepo.find();
     res.send(labels);
