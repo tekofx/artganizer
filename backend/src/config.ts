62246de92de9e61573e3be5b7b3f0dcfc9187907
tekofx <tekofxx@gmail.com>
2023-08-03 18:39:11 +0200

    Using config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,20 +1,26 @@
 import "dotenv/config";
 interface Config {
-    URL: string;
+  URL: string;
+  MYSQL_DATABASE: string;
+  MYSQL_USER: string;
+  MYSQL_PASSWORD: string;
+  MYSQL_HOST: string;
 }
 
 let config: Config;
 
-
-
 try {
-    config = require("../config.json");
-    console.log("Using config.json");
+  config = require("../config.json");
+  console.log("Using config.json");
 } catch (error) {
-    console.log("Using environment variables");
-    config = {
-        URL: process.env.URL || "localhost:3001",
-    };
+  console.log("Using environment variables");
+  config = {
+    URL: process.env.URL || "localhost:3001",
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE || "db",
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD || "1234",
+    MYSQL_USER: process.env.MYSQL_USER || "teko",
+    MYSQL_HOST: process.env.MYSQL_HOST || "localhost"
+  };
 }
 
 export { config };
