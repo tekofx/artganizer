b4178cafb71ea5ddd6842b033ad63ba8d14bc9ea
tekofx <tekofxx@gmail.com>
2023-08-22 10:27:29 +0200

    Added creation of settings.json

--- backend/src/routes/settings.ts
+++ backend/src/routes/settings.ts
@@ -4,7 +4,21 @@ import path from "path";
 import fs from "fs";
 
 const router = express.Router();
+const defaultSettings = {
+  galleryInfo: {
+    title: true,
+    tags: false,
+    characters: false,
+    artist: false,
+    dimensions: true,
+    date: false,
+    colors: false,
+  },
+};
 const settingsFile = path.join(__dirname, "../../data/settings.json");
+if (!fs.existsSync(settingsFile)) {
+  fs.writeFileSync(settingsFile, JSON.stringify(defaultSettings));
+}
 
 router.get("/", async (req: Request, res: Response) => {
   // Send content of settings.json
