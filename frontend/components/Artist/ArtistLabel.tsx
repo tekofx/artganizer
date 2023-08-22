e325e28eb10d4ef02718a8f9a5769560af544dec
tekofx <tekofxx@gmail.com>
2023-08-22 11:34:53 +0200

    Removed menuitem

--- frontend/components/Artist/ArtistLabel.tsx
+++ frontend/components/Artist/ArtistLabel.tsx
@@ -1,29 +1,51 @@
-import { Stack, Avatar, Typography, MenuItem } from "@mui/material";
+import { Stack, Avatar, Typography } from "@mui/material";
 import Artist from "../../interfaces/Artist";
 import { useRouter } from "next/router";
+import { useState } from "react";
 interface ArtistLabelProps {
   artist: Artist;
   clickable?: boolean;
 }
 export default function Page(props: ArtistLabelProps) {
   const router = useRouter();
+  const [highlight, setHighlight] = useState(false);
 
   function handleClick() {
     if (props.clickable) {
       router.push(`/artist/${props.artist.id}`);
     }
   }
+
+  function handleMouseEnter() {
+    if (props.clickable) {
+      setHighlight(true);
+    }
+  }
+
+  function handleMouseLeave() {
+    if (props.clickable) {
+      setHighlight(false);
+    }
+  }
+
   return (
-    <MenuItem
+    <Stack
+      direction="row"
+      alignItems="center"
+      spacing={2}
       onClick={handleClick}
-      sx={{ cursor: props.clickable ? "pointer" : "default" }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        cursor: props.clickable ? "pointer" : "default",
+        p: 1,
+        backgroundColor: highlight ? "grey" : "transparent",
+      }}
     >
-      <Stack direction="row" alignItems="center" spacing={2}>
-        <Avatar
-          src={`http://localhost:3001/artists/uploads/${props.artist.id}`}
-        />
-        <Typography>{props.artist.name}</Typography>
-      </Stack>
-    </MenuItem>
+      <Avatar
+        src={`http://localhost:3001/artists/uploads/${props.artist.id}`}
+      />
+      <Typography>{props.artist.name}</Typography>
+    </Stack>
   );
 }
