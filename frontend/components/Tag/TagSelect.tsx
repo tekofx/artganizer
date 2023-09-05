371924e7a8b59c7f07d97934308499e9000c3891
tekofx <tekofxx@gmail.com>
2023-09-05 13:00:06 +0200

    Added option to create a tag

--- frontend/components/Tag/TagSelect.tsx
+++ frontend/components/Tag/TagSelect.tsx
@@ -6,16 +6,23 @@ import {
   Popper,
   Stack,
   Grid,
+  IconButton,
 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import SelectableTagList from "./SelectableTagList";
 import Tag from "../../interfaces/Tag";
 import TagChip from "./TagChip";
+import { TagForm } from "../Forms";
 interface TagSelectProps {
   setSelectedTags: Dispatch<SetStateAction<Tag[]>>;
   selectedTags: Tag[];
 }
-export default function TagSelect(props: TagSelectProps) {
+export default function TagSelect({
+  selectedTags,
+  setSelectedTags,
+}: TagSelectProps) {
   const [open, setOpen] = useState(false);
+  const [openTagForm, setOpenTagForm] = useState(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const [filter, setFilter] = useState<string>("");
@@ -28,34 +35,37 @@ export default function TagSelect(props: TagSelectProps) {
   };
 
   function removeTag(tag: Tag) {
-    props.setSelectedTags(props.selectedTags.filter((t) => t.id !== tag.id));
+    setSelectedTags(selectedTags.filter((t) => t.id !== tag.id));
   }
 
   return (
-    <Grid container>
+    <Grid container spacing={1}>
       <Grid item xs={12}>
         <Typography>Tags select</Typography>
       </Grid>
-      <Grid container spacing={1}>
-        {props.selectedTags.map((tag) => (
-          <Grid item key={tag.id}>
-            <Stack direction="row" spacing={2} alignItems="center">
-              <TagChip tag={tag} onDelete={() => removeTag(tag)} />
-            </Stack>
-          </Grid>
-        ))}
+      <Grid item xs={12}>
+        <Grid container spacing={1}>
+          {selectedTags.map((tag) => (
+            <Grid item key={tag.id}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <TagChip tag={tag} onDelete={() => removeTag(tag)} />
+              </Stack>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
 
-      {props.selectedTags.length < 1 && (
+      {selectedTags.length < 1 && (
         <>
           <Typography>No tags selected</Typography>
         </>
       )}
-      <Grid item xs={12}>
+      <Grid item xs={10}>
         <TextField
           label="Search tags"
           variant="standard"
           size="small"
+          fullWidth
           value={filter}
           onClick={handleClick}
           onChange={(event) => {
@@ -63,16 +73,22 @@ export default function TagSelect(props: TagSelectProps) {
           }}
         />
       </Grid>
+      <Grid item xs={2}>
+        <IconButton onClick={() => setOpenTagForm(true)}>
+          <AddIcon />
+        </IconButton>
+      </Grid>
       <Popper open={open} anchorEl={anchorEl} sx={{ zIndex: 2000 }}>
         <Paper sx={{ width: "200px" }}>
           <SelectableTagList
-            selectedTags={props.selectedTags}
-            setSelectedTags={props.setSelectedTags}
+            selectedTags={selectedTags}
+            setSelectedTags={setSelectedTags}
             setOpen={setOpen}
             filter={filter}
           />
         </Paper>
       </Popper>
+      <TagForm open={openTagForm} setOpen={setOpenTagForm} />
     </Grid>
   );
 }
