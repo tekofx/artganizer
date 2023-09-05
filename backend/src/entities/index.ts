2fc6670a5cd0b49b55c209ce588b22d8516c543a
tekofx <tekofxx@gmail.com>
2023-09-05 11:01:01 +0200

    moved to index

---
+++ backend/src/entities/index.ts
@@ -0,0 +1,6 @@
+import { Submission } from "./Submission";
+import { Tag } from "./Tag";
+import { Character } from "./Character";
+import { Folder } from "./Folder";
+import { Artist } from "./Artist";
+export { Artist, Submission, Tag, Character, Folder };
