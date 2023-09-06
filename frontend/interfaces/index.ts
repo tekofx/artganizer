b513468b9cd640ed25724d358b4e02a42b3029a3
tekofx <tekofxx@gmail.com>
2023-09-06 10:53:23 +0200

    Moved components

---
+++ frontend/interfaces/index.ts
@@ -0,0 +1,19 @@
+import AlertMessage from "./AlertMessage";
+import Artist from "./Artist";
+import Character from "./Character";
+import Folder from "./Folder";
+import Tag from "./Tag";
+import Settings from "./Settings";
+import Social from "./Social";
+import Submission from "./Submission";
+
+export type {
+  AlertMessage,
+  Artist,
+  Character,
+  Folder,
+  Tag,
+  Settings,
+  Social,
+  Submission,
+};
