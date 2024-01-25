6a7974a5730f626f59f5be1df5590be2e3666d17
tekofx <tekofxx@gmail.com>
2024-01-25 13:25:30 +0100

    Improvements when creating tag

--- frontend/components/Tag/SelectableTagList.tsx
+++ frontend/components/Tag/SelectableTagList.tsx
@@ -9,24 +9,34 @@ interface TagListProps {
   setSelectedTags: Dispatch<SetStateAction<Tag[]>>;
 }
 
+const colors = ["#7BDCB5", "#FCB900", "#FF6900", "#0693E3", "#8ED1FC", "#F78DA7", "#EB144C", "#ABB8C3", "#9900EF"]
+
 export default function SelectableTagList({
   setSelectedTags,
   selectedTags,
 }: TagListProps) {
   const [inputValue, setInputValue] = useState("");
   const [tags, setTags] = useState<Tag[]>([]);
+  const getTags = async () => {
+    var res = await axios.get(process.env.API_URL + "/tags");
+    setTags(res.data);
+  };
   useEffect(() => {
-    const getTags = async () => {
-      var res = await axios.get(process.env.API_URL + "/tags");
-      setTags(res.data);
-    };
     getTags();
-  }
-
-    , []);
+  }, []);
 
   function handleDeleteTag(tag: Tag) {
     setSelectedTags(selectedTags.filter((t) => t.id !== tag.id));
+  }
+
+  async function onAddTag() {
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    await axios.post(process.env.API_URL + `/tags`, { name: inputValue, color: randomColor }).then((res) => {
+      setSelectedTags([...selectedTags, res.data]);
+      setInputValue("");
+      getTags();
+    }
+    );
   }
 
   return (
@@ -47,6 +57,10 @@ export default function SelectableTagList({
           <TextField {...params} variant="outlined" label="Tags" />
         )}
         renderOption={(props, tag) => {
+          const isSelected = selectedTags.some((selectedTag) => selectedTag.id === tag.id);
+          if (isSelected) {
+            props["aria-selected"] = true;
+          }
           return (
             <li {...props}>
               <TagLabel tag={tag} />
@@ -62,7 +76,7 @@ export default function SelectableTagList({
             />
           ))
         }
-        noOptionsText={<Button>Create tag {inputValue}</Button>}
+        noOptionsText={<Button onClick={onAddTag}>Create tag {inputValue}</Button>}
       />
     </>
   );
