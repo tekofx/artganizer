ea4df3724d67bb53308027a80159a422c6446d13
tekofx <tekofxx@gmail.com>
2024-01-30 22:26:46 +0100

    Changes in layout

--- frontend/components/Layout/Panels/BottomAppBar.tsx
+++ frontend/components/Layout/Panels/BottomAppBar.tsx
@@ -1,14 +1,13 @@
-import MoreIcon from "@mui/icons-material/MoreVert";
+import BrushIcon from "@mui/icons-material/Brush";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import PersonIcon from "@mui/icons-material/Person";
+import PhotoIcon from "@mui/icons-material/Photo";
 import SearchIcon from "@mui/icons-material/Search";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
-import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import { styled } from "@mui/material/styles";
-import Menu from "../Menu";
-
 const StyledFab = styled(Fab)({
   position: "absolute",
   zIndex: 1,
@@ -20,25 +19,29 @@ const StyledFab = styled(Fab)({
 
 export default function BottomAppBar() {
   return (
-    <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
-      <Toolbar>
-        <BottomNavigation showLabels>
-          <BottomNavigationAction label="Artists" icon={<SearchIcon />} />
-          <BottomNavigationAction label="Characters" icon={<SearchIcon />} />
-          <BottomNavigationAction label="Submissions" icon={<SearchIcon />} />
-          <BottomNavigationAction label="Tags" icon={<SearchIcon />} />
+    <AppBar
+      position="fixed"
+      color="primary"
+      sx={{ top: "auto", bottom: 0, paddingLeft: 0, paddingRight: 0 }}
+    >
+      <Toolbar sx={{ top: "auto", bottom: 0, paddingLeft: 0, paddingRight: 0 }}>
+        <BottomNavigation showLabels sx={{ width: "100%" }}>
+          <BottomNavigationAction label="Artists" icon={<BrushIcon />} />
+          <BottomNavigationAction label="Characters" icon={<PersonIcon />} />
+          <BottomNavigationAction label="Submissions" icon={<PhotoIcon />} />
+          <BottomNavigationAction label="Tags" icon={<LocalOfferIcon />} />
         </BottomNavigation>
-        <Menu />
         <StyledFab color="secondary" aria-label="add">
           <SearchIcon />
         </StyledFab>
-        <Box sx={{ flexGrow: 1 }} />
+        {/* <Box sx={{ flexGrow: 1 }} />
         <IconButton color="inherit">
-          <SearchIcon />
+        <SearchIcon />
         </IconButton>
         <IconButton color="inherit">
-          <MoreIcon />
-        </IconButton>
+        <MoreIcon />
+      </IconButton>
+      <Menu /> */}
       </Toolbar>
     </AppBar>
   );
