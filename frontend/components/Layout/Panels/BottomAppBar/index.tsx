540bb7710d79fdf68fc3a6d6dd002e0b308f19da
tekofx <tekofxx@gmail.com>
2024-01-31 15:31:04 +0100

    Fix hydratation error

--- frontend/components/Layout/Panels/BottomAppBar/index.tsx
+++ frontend/components/Layout/Panels/BottomAppBar/index.tsx
@@ -4,48 +4,38 @@ import PersonIcon from "@mui/icons-material/Person";
 import PhotoIcon from "@mui/icons-material/Photo";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import Fab from "@mui/material/Fab";
 import Toolbar from "@mui/material/Toolbar";
-import { styled } from "@mui/material/styles";
 import { useRouter } from "next/router";
-import { useEffect, useState } from 'react';
-import CreatePopup from './CreatePopup';
-const StyledFab = styled(Fab)({
-  position: "absolute",
-  zIndex: 1,
-  top: -30,
-  left: 0,
-  right: 0,
-  margin: "0 auto",
-});
+import { useEffect, useState } from "react";
+import CreatePopup from "./CreateButton";
 
 export default function BottomAppBar() {
   const router = useRouter();
   const [value, setValue] = useState(0);
 
-
   const pages = [
     { id: 0, page: "Artists", navigate: "/artists", icon: <BrushIcon /> },
-    { id: 1, page: "Characters", navigate: "/characters", icon: <PersonIcon /> },
+    {
+      id: 1,
+      page: "Characters",
+      navigate: "/characters",
+      icon: <PersonIcon />,
+    },
     { id: 2, page: "Submissions", navigate: "/", icon: <PhotoIcon /> },
     { id: 3, page: "Tags", navigate: "/tags", icon: <LocalOfferIcon /> },
-  ]
-
+  ];
 
   useEffect(() => {
     function getIdFromPath(path: string) {
-      const page = pages.find(page => page.navigate === path);
+      const page = pages.find((page) => page.navigate === path);
       if (page) {
         return page.id;
       }
       return 0;
-
     }
-    console.log(router.pathname)
+    console.log(router.pathname);
     setValue(getIdFromPath(router.pathname));
-
   }, []);
-
 
   function onClick(event: React.ChangeEvent<{}>, newValue: number) {
     setValue(newValue);
@@ -68,17 +58,8 @@ export default function BottomAppBar() {
             />
           ))}
         </BottomNavigation>
-        <StyledFab color="secondary" aria-label="add">
-          <CreatePopup />
-        </StyledFab>
-        {/* <Box sx={{ flexGrow: 1 }} />
-        <IconButton color="inherit">
-        <SearchIcon />
-        </IconButton>
-        <IconButton color="inherit">
-        <MoreIcon />
-      </IconButton>
-      <Menu /> */}
+
+        <CreatePopup />
       </Toolbar>
     </AppBar>
   );
