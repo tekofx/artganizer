6bcb280621b5ca51fce7ab0e154a9efd8d024480
tekofx <tekofxx@gmail.com>
2024-02-05 14:01:03 +0100

    Changes on login

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -1,7 +1,26 @@
+import bcrypt from "bcrypt";
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { UserRepo } from "../typeorm.config";
 const router = express.Router();
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  await UserRepo.findOne({ where: { username: req.body.username } })
+    .then((user) => {
+      bcrypt.compare(req.body.password, user.password).then((passwordMatches) => {
+       if (!passwordMatches){
+        return res.status(500).send("Password does not match");
+       }
+
+       const token=jwt.sign({username:user.username},process.env.BACKEND_SECRET_KEY,{expiration:60*60});
+
+      }
+
+
+    })
+    .catch((e) => {
+      res.status(500).send("User not found");
+    });
+
   console.log(req.session);
   res.send("Hello world");
 });
