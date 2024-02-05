7d22291e2523162289ed0ac2c05a20baef32331f
tekofx <tekofxx@gmail.com>
2024-02-05 13:05:09 +0100

    Moved to index

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+import artist from "./artists";
+import characters from "./characters";
+import login from "./login";
+import register from "./register";
+import settings from "./settings";
+import submission from "./submissions";
+import tags from "./tags";
+
+export { artist, characters, login, register, settings, submission, tags };
