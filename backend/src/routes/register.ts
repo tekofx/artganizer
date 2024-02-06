94d1644fae27fdadf56f42951783323da3ad78c1
tekofx <tekofxx@gmail.com>
2024-02-06 09:29:15 +0100

    Fixes in register endpoint

--- backend/src/routes/register.ts
+++ backend/src/routes/register.ts
@@ -4,16 +4,27 @@ import { UserRepo } from "../typeorm.config";
 
 const router = express.Router();
 router.post("/", async (req, res) => {
-  try {
-    var { username, password } = req.body;
-    bcrypt.hash(password, 10, async function (err, hash) {
-      // Store hash in your password DB.
-      await UserRepo.save({ username: username, password: hash });
-    });
-    return res.send("User created successfully!");
-  } catch (e) {
-    return res.status(500).send("Error creating user");
+  var { username, password } = req.body;
+  const existsUser = await UserRepo.findOne({ where: { username: username } });
+  if (existsUser) {
+    return res.status(400).send("User already exists");
   }
+
+  bcrypt.hash(password, 10).then(async (hashedPassword) => {
+    // Store hash in your password DB.
+    console.log("createing user...");
+    await UserRepo.save({
+      username: username,
+      password: hashedPassword,
+    })
+      .catch((e) => {
+        console.log(e);
+        return res.status(500).send("Error creating user");
+      })
+      .then(() => {
+        return res.send("User created");
+      });
+  });
 });
 
 export default router;
