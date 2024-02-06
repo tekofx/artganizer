a3e9d1713b36144ed3faf1d8be2bb52a434acc0d
tekofx <tekofxx@gmail.com>
2024-02-06 09:39:27 +0100

    Fixes in login

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -1,28 +1,34 @@
 import bcrypt from "bcrypt";
 import express from "express";
 import jwt from "jsonwebtoken";
+import { config } from "../config";
 import { UserRepo } from "../typeorm.config";
 const router = express.Router();
 router.post("/", async (req, res) => {
-  await UserRepo.findOne({ where: { username: req.body.username } })
-    .then((user) => {
-      bcrypt.compare(req.body.password, user.password).then((passwordMatches) => {
-       if (!passwordMatches){
-        return res.status(500).send("Password does not match");
-       }
-
-       const token=jwt.sign({username:user.username},process.env.BACKEND_SECRET_KEY,{expiration:60*60});
-
+  await UserRepo.findOne({ where: { username: req.body.username } }).then(
+    (user) => {
+      if (user) {
+        bcrypt.compare(req.body.password, user.password).then((result) => {
+          if (result) {
+            const token = jwt.sign(
+              { username: user.username },
+              config.BACKEND_SECRET_KEY,
+              { expiresIn: "24h" }
+            );
+            return res.send({
+              message: "Login succesful",
+              username: user.username,
+              token: token,
+            });
+          } else {
+            return res.status(401).send("Invalid password");
+          }
+        });
+      } else {
+        return res.status(401).send("Invalid username");
       }
-
-
-    })
-    .catch((e) => {
-      res.status(500).send("User not found");
-    });
-
-  console.log(req.session);
-  res.send("Hello world");
+    }
+  );
 });
 
 export default router;
