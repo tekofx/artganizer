dd6f0144b817145a97f2b2b7fc8067d73c7b7e4a
tekofx <tekofxx@gmail.com>
2024-02-12 11:41:45 +0100

    added filters to mobile view

--- frontend/components/Layout/Panels/TopPanel/index.tsx
+++ frontend/components/Layout/Panels/TopPanel/index.tsx
@@ -11,53 +11,48 @@ import {
   TagFilter
 } from "./Filters";
 export default function TopPanel() {
-  const { filters, setFilters, isMobile } = useAppContext();
+  const { filters, setFilters } = useAppContext();
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
     setFilters({ ...filters, title: event.target.value });
   }
   return (
     <>
-      {!isMobile ?
-        (
 
-          <Paper
-            sx={{
-              width: "100%",
-              p: 1,
-              position: "sticky",
-              top: 0,
-              zIndex: 4000,
-              display: "block",
-            }}
-          >
-            <Grid container spacing={1}>
-              <Grid item >
-                <SearchBar onChange={onChange} show />
-              </Grid>
-              <Grid item>
-                <RatingFilter />
-              </Grid>
-              <Grid item>
-                <TagFilter />
-              </Grid>
-              <Grid item>
-                <ColorFilter />
-              </Grid>
-              <Grid item>
-                <ArtistFilter />
-              </Grid>
-              <Grid item>
-                <CharacterFilter />
-              </Grid>
-              <Grid item>
-                <ClearFilters />
-              </Grid>
-            </Grid>
-          </Paper>
-        ) :
-        null
-      }
+      <Paper
+        sx={{
+          width: "100%",
+          p: 1,
+          position: "sticky",
+          top: 0,
+          zIndex: 4000,
+          display: "block",
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid item>
+            <SearchBar onChange={onChange} show />
+          </Grid>
+          <Grid item>
+            <RatingFilter />
+          </Grid>
+          <Grid item>
+            <TagFilter />
+          </Grid>
+          <Grid item>
+            <ColorFilter />
+          </Grid>
+          <Grid item>
+            <ArtistFilter />
+          </Grid>
+          <Grid item>
+            <CharacterFilter />
+          </Grid>
+          <Grid item>
+            <ClearFilters />
+          </Grid>
+        </Grid>
+      </Paper >
 
     </>
 
