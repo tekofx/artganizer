569d2ebb6dd6ced66583f6df59882ef28608b8d5
tekofx <tekofxx@gmail.com>
2024-02-12 11:30:03 +0100

    Responsive filters

--- frontend/components/Layout/Panels/TopPanel/index.tsx
+++ frontend/components/Layout/Panels/TopPanel/index.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 
 import { useAppContext } from "../../../../pages/_app";
 import SearchBar from "../../../SearchBar";
@@ -11,31 +11,55 @@ import {
   TagFilter
 } from "./Filters";
 export default function TopPanel() {
-  const { filters, setFilters } = useAppContext();
+  const { filters, setFilters, isMobile } = useAppContext();
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
     setFilters({ ...filters, title: event.target.value });
   }
   return (
-    <Paper
-      sx={{
-        width: "100%",
-        p: 1,
-        position: "sticky",
-        top: 0,
-        zIndex: 4000,
-        display: { xs: "none", sm: "none", md: "none", lg: "block" },
-      }}
-    >
-      <Stack direction="row" spacing={1}>
-        <SearchBar onChange={onChange} show />
-        <RatingFilter />
-        <TagFilter />
-        <ColorFilter />
-        <ArtistFilter />
-        <CharacterFilter />
-        <ClearFilters />
-      </Stack>
-    </Paper>
+    <>
+      {!isMobile ?
+        (
+
+          <Paper
+            sx={{
+              width: "100%",
+              p: 1,
+              position: "sticky",
+              top: 0,
+              zIndex: 4000,
+              display: "block",
+            }}
+          >
+            <Grid container spacing={1}>
+              <Grid item >
+                <SearchBar onChange={onChange} show />
+              </Grid>
+              <Grid item>
+                <RatingFilter />
+              </Grid>
+              <Grid item>
+                <TagFilter />
+              </Grid>
+              <Grid item>
+                <ColorFilter />
+              </Grid>
+              <Grid item>
+                <ArtistFilter />
+              </Grid>
+              <Grid item>
+                <CharacterFilter />
+              </Grid>
+              <Grid item>
+                <ClearFilters />
+              </Grid>
+            </Grid>
+          </Paper>
+        ) :
+        null
+      }
+
+    </>
+
   );
 }
