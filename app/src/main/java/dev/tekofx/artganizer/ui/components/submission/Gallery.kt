287ba759600b9527b58d28b0d941b9e4ca20074f
tekofx <tekofxx@gmail.com>
2025-05-17 19:26:28 +0200

    Fixes in grid

--- app/src/main/java/dev/tekofx/artganizer/ui/components/submission/Gallery.kt
+++ app/src/main/java/dev/tekofx/artganizer/ui/components/submission/Gallery.kt
@@ -1,11 +1,18 @@
 package dev.tekofx.artganizer.ui.components.submission
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -16,18 +23,24 @@ import dev.tekofx.artganizer.navigation.NavigateDestinations
 fun Gallery(navHostController: NavHostController, submissions: List<Submission>) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
+
         content = {
             items(submissions) { submission ->
-                Card(
-                    onClick = { navHostController.navigate("${NavigateDestinations.SUBMISSIONS_SCREEN}/${submission.id}") }
-                ) {
-                    AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
-                            .data(submission.imagePath)
-                            .build(),
-                        contentDescription = "icon",
-                    )
-                }
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(submission.imagePath)
+                        .build(),
+                    contentDescription = submission.title,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .aspectRatio(1f)
+                        .padding(2.dp)
+                        .clip(MaterialTheme.shapes.small)
+                        .clickable {
+                            navHostController.navigate("${NavigateDestinations.SUBMISSIONS_SCREEN}/${submission.id}")
+                        }
+
+                )
             }
         }
     )
