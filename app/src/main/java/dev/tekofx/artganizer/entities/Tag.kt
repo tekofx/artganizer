bd3034888ff65ef9aa639c559b8781bcffc80059
tekofx <tekofxx@gmail.com>
2025-06-01 13:03:34 +0200

    Make tag name unique

--- app/src/main/java/dev/tekofx/artganizer/entities/Tag.kt
+++ app/src/main/java/dev/tekofx/artganizer/entities/Tag.kt
@@ -1,9 +1,10 @@
 package dev.tekofx.artganizer.entities
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tags")
+@Entity(tableName = "tags", indices = [Index(value = ["name"], unique = true)])
 data class Tag(
     @PrimaryKey(autoGenerate = true)
     val tagId: Long = 0,
