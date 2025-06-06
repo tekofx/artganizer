977e6ffde6e10a0c5d626bac4449fb2a982e3bd1
tekofx <tekofxx@gmail.com>
2025-06-06 13:57:13 +0200

    Improvements in searchbar positioning

--- app/src/main/java/dev/tekofx/artganizer/ui/screens/artists/ArtistsScreen.kt
+++ app/src/main/java/dev/tekofx/artganizer/ui/screens/artists/ArtistsScreen.kt
@@ -2,16 +2,18 @@ package dev.tekofx.artganizer.ui.screens.artists
 
 
 import android.annotation.SuppressLint
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector2D
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -30,18 +32,26 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.round
 import androidx.navigation.NavHostController
 import dev.tekofx.artganizer.navigation.NavigateDestinations
 import dev.tekofx.artganizer.ui.components.artists.ArtistCard
 import dev.tekofx.artganizer.ui.components.buttons.CreateFab
 import dev.tekofx.artganizer.ui.components.input.ThinSearchBar
 import dev.tekofx.artganizer.ui.viewmodels.artists.ArtistsViewModel
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
@@ -68,8 +78,20 @@ fun ArtistScreen(
 
     // Animate offset based on focus state
     val searchBarOffset by animateDpAsState(
-        if (isSearchBarFocused.value) 20.dp else screenHeightDp - 160.dp
+        if (isSearchBarFocused.value) -screenHeightDp + 150.dp else 0.dp
     )
+    var alignment by remember {
+        mutableStateOf(Alignment.Center)
+    }
+
+    LaunchedEffect(isSearchBarFocused.value) {
+        if (isSearchBarFocused.value) {
+            alignment = Alignment.TopCenter
+        } else {
+            alignment = Alignment.BottomCenter
+        }
+    }
+
     // Monitor keyboard visibility
     val isImeVisible = WindowInsets.isImeVisible
     LaunchedEffect(isImeVisible) {
@@ -79,14 +101,16 @@ fun ArtistScreen(
         }
     }
 
+
+
+
+
     BottomSheetScaffold(
-        scaffoldState = scaffoldState,
-        sheetContent = {
+        scaffoldState = scaffoldState, sheetContent = {
             Column {
                 Text("Sheet")
             }
-        }
-    ) {
+        }) {
         Scaffold(
             floatingActionButton = {
                 if (!isSearchBarFocused.value && artistsViewModel.state.text.isEmpty()) {
@@ -96,32 +120,30 @@ fun ArtistScreen(
                 }
             },
         ) {
-            Box(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
                 LazyColumn(
                     verticalArrangement = Arrangement.spacedBy(10.dp),
                     modifier = Modifier
-                        .padding(10.dp),
+                        .padding(
+                            top = if (isSearchBarFocused.value) 50.dp else 0.dp,
+                            bottom = if (!isSearchBarFocused.value) 50.dp else 0.dp,
+                            start = 10.dp,
+                            end = 10.dp
+                        ),
                 ) {
-                    if (isSearchBarFocused.value) {
-                        item {
-                            Spacer(modifier = Modifier.height(60.dp))
-                        }
-                    }
+
 
                     items(artists) { artist ->
                         ArtistCard(
-                            artist,
-                            onClick = {
+                            artist, onClick = {
                                 navHostController.navigate("${NavigateDestinations.ARTISTS_SCREEN}/${artist.artist.artistId}")
-                            },
-                            modifier = Modifier.animateItem()
+                            }, modifier = Modifier.animateItem()
                         )
                     }
-                    if (!isSearchBarFocused.value) {
-                        item {
-                            Spacer(modifier = Modifier.height(40.dp))
-                        }
-                    }
+
 
                 }
                 ThinSearchBar(
@@ -130,12 +152,44 @@ fun ArtistScreen(
                     onFocusChanged = { isSearchBarFocused.value = it },
                     focusManager = focusManager,
                     modifier = Modifier
-                        .align(Alignment.TopCenter)
-                        .offset(y = searchBarOffset)
+                        .align(alignment)
+                        .animatePlacement()
                         .padding(horizontal = 10.dp)
-                        .animateContentSize()
                 )
             }
         }
     }
+}
+
+fun Modifier.animatePlacement(): Modifier = composed {
+    val scope = rememberCoroutineScope()
+    var targetOffset by remember { mutableStateOf(IntOffset.Zero) }
+    var animatable by remember {
+        mutableStateOf<Animatable<IntOffset, AnimationVector2D>?>(null)
+    }
+    this
+        // 🔥 onPlaced should be before offset Modifier
+        .onPlaced {
+            // Calculate the position in the parent layout
+            targetOffset = it
+                .positionInParent()
+                .round()
+        }
+        .offset {
+            // Animate to the new target offset when alignment changes.
+            val anim = animatable ?: Animatable(targetOffset, IntOffset.VectorConverter)
+                .also {
+                    animatable = it
+                }
+
+
+            if (anim.targetValue != targetOffset) {
+                scope.launch {
+                    anim.animateTo(targetOffset, spring(stiffness = Spring.StiffnessMediumLow))
+                }
+            }
+            // Offset the child in the opposite direction to the targetOffset, and slowly catch
+            // up to zero offset via an animation to achieve an overall animated movement.
+            animatable?.let { it.value - targetOffset } ?: IntOffset.Zero
+        }
 }
