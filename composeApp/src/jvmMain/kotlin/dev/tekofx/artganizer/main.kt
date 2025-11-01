5bdcc88f1bf24f15326f8746b52328a09a885bd6
tekofx <tekofxx@gmail.com>
2025-11-01 13:51:54 +0100

    [Desktop] Changed Application Id for FileKit

--- composeApp/src/jvmMain/kotlin/dev/tekofx/artganizer/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/tekofx/artganizer/main.kt
@@ -3,11 +3,10 @@ package dev.tekofx.artganizer
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import io.github.vinceglb.filekit.FileKit
-import org.koin.core.context.startKoin
 
 
 fun main() {
-    FileKit.init(appId = "MyApplication")
+    FileKit.init(appId = "Artganizer")
     application {
         initKoin {
             modules()
